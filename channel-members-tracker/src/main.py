55701bfb5b620e32366a8f305e303bcaaba21da5
Daniil <daniil@vivaldi.net>
2022-10-09 20:34:30 +0600

    Dotenv and first tries in telethon

--- channel-members-tracker/src/main.py
+++ channel-members-tracker/src/main.py
@@ -1,2 +1,17 @@
+import os
+
+from telethon import TelegramClient, events, sync
+from dotenv import load_dotenv
+
+
 def launch():
-    print('Works')
+    load_dotenv()
+    test()
+
+
+def test():
+    api_id = int(os.getenv('API_ID'))
+    api_hash = os.getenv('API_HASH')
+
+    with TelegramClient('anon', api_id, api_hash) as client:
+        client.loop.run_until_complete(client.send_message('me', 'Hello, myself!'))
